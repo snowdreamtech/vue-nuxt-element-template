6ce78a74ce0ae0abe936ed4e3418227ee6ffd268
sn0wdr1am <sn0wdr1am@qq.com>
2023-08-30 18:02:24 +0800

    Fix: SideBar Logo TItle can not be shown.

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="{ 'has-logo': showLogo }">
-    <sidebar-logo v-if="showLogo" :collapse="true" />
+    <sidebar-logo v-if="showLogo" :collapse="isCollapse" />
     <el-scrollbar wrap-class="scrollbar-wrapper">
       <el-menu :default-active="activeMenu" :collapse="isCollapse" :background-color="menuBg" :text-color="menuText"
         :unique-opened="false" :active-text-color="menuActiveText" :collapse-transition="false" mode="vertical">
