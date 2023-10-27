4ee7a56c354f7ba757957aa3cfa9143a0c630907
sn0wdr1am <sn0wdr1am@qq.com>
2023-10-27 19:40:50 +0800

    fix:  Nuxt Build Error: No known conditions for "./node" specifier in "msw" package
    fixed #2

--- mocks/browser.ts
+++ mocks/browser.ts
@@ -1,4 +1,14 @@
 import { setupWorker } from 'msw/browser'
+import path from 'path-browserify'
 import { handlers } from './handlers'
 
 export const worker = setupWorker(...handlers)
+
+export function mockBrowser (config:any) {
+  worker.start({
+    serviceWorker: {
+      url: path.resolve(config.baseURL, 'mockServiceWorker.js')
+    },
+    onUnhandledRequest: 'bypass'
+  })
+}
